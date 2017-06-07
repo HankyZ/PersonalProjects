40b2b5250c02a115562b11800a59a05be6d1ad08
Hank <hankyzhang@gmail.com>
2017-06-06 23:17:59 -0400

    added comments

--- Battle of Gods/src/ClassicBoard.java
+++ Battle of Gods/src/ClassicBoard.java
@@ -3,7 +3,10 @@ public class ClassicBoard extends Board {
 	private Case[][] board;
 	
 	public ClassicBoard() {
+		
 			super();
+			
+			// create a board with water tiles in the middle		
 			for ( int x = 0; x < 9; x++ ) {
 				this.getCase(x, 4).setEnv(1);
 			}
--- Battle of Gods/src/Display.java
+++ Battle of Gods/src/Display.java
@@ -19,16 +19,20 @@ public class Display extends JFrame{
 	private JPanel topPanel;
 	private JPanel midPanel;
 	private JPanel botPanel;
+	
 	private JPanel gamePanel;
+	
 	private JLayeredPane[][] gameCases;
 	private JLabel[][] gameEnv;
 	private JLabel[][] gameGods;
+	
 	private Board board;
+	
 	private Icon[] tiles;
 	private Icon[] gods;
 	
+	// constructor used for testing
 	public Display(Board board) {
-
 		super("Battle of Gods");
 		tiles = new Icon[5];
 		gods = new Icon[5];
@@ -39,6 +43,7 @@ public class Display extends JFrame{
 		refreshBoard();
 	}
 	
+	// default constructor
 	public Display() {
 		super("Battle of Gods");
 		tiles = new Icon[5];
@@ -50,15 +55,15 @@ public class Display extends JFrame{
 		refreshBoard();
 	}
 	
+	// initialize game
 	private void newGame(Board board) {
-		
 		
 		this.board = board;
 		
+		// create and add the three core panels
 		topPanel = new JPanel();
 		topPanel.setBackground(Color.ORANGE);
 		topPanel.setPreferredSize(new Dimension(100, 20));
-		
 		
 		midPanel = new JPanel();
 		midPanel.setBackground(Color.BLUE);
@@ -71,23 +76,33 @@ public class Display extends JFrame{
 		add(midPanel);
 		add(botPanel, BorderLayout.SOUTH);
 		
+		// add the gamePanel to the midPanel
 		gamePanel = new JPanel(new GridLayout(9, 9));
 		gamePanel.setBackground(Color.WHITE);
 		gamePanel.setPreferredSize(new Dimension(900, 900));
 		midPanel.add(gamePanel, BorderLayout.NORTH);
 		
-		
+		// add the gameCases to the gamePanel
 		gameCases = new JLayeredPane[9][9];
 		gameEnv = new JLabel[9][9];
 		gameGods = new JLabel[9][9];
 		
 		for ( int x = 0; x < getBoard().getsize(); x++ ) {
 			for ( int y = 0; y < getBoard().getsize(); y++ ) {
+				
 				gameCases[y][x] = new JLayeredPane();
 				gameCases[y][x].setPreferredSize(new Dimension(100, 100));
+				
+
+				gameGods[y][x] = new JLabel();
+				gameCases[y][x].add(gameGods[y][x], 0);
+				gameGods[y][x].setBounds(0, 0, 100, 100);
+				
 				gameEnv[y][x] = new JLabel();
 				gameEnv[y][x].setBounds(0, 0, 100, 100);
+				gameEnv[y][x].setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLACK));
 				gameCases[y][x].add(gameEnv[y][x], 1);
+				
 				switch ( board.getCase(y, x).getEnv() ) {
 					case 0 :
 						gameEnv[y][x].setIcon(tiles[0]);
@@ -98,15 +113,8 @@ public class Display extends JFrame{
 					default :
 						break;
 				}
-				gameEnv[y][x].setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLACK));
 
-
-				gameGods[y][x] = new JLabel();
-				gameCases[y][x].add(gameGods[y][x], 0);
-				gameGods[y][x].setBounds(0, 0, 100, 100);
-				
 				gameCases[y][x].addMouseListener(new MouseAdapter() {
-					
 					public void mouseClicked(MouseEvent e){
 						
 						for( int x = 0; x < getBoard().getsize(); x++ ){
@@ -118,8 +126,7 @@ public class Display extends JFrame{
 										getBoard().getCase(y, x).setOccupied(1);
 									}
 									else {
-										getBoard().getCase(y, x).setOccupied(0);
-										gameGods[y][x].setIcon(null);
+										
 									}
 								}
 							}
@@ -128,7 +135,6 @@ public class Display extends JFrame{
 				});
 				gamePanel.add(gameCases[y][x]);
 				
-
 			}
 		}
 	}
@@ -143,14 +149,12 @@ public class Display extends JFrame{
 			for ( int x = 0; x < 9; x++ ) {
 				switch ( board.getCase(x, y).getOccupied() ) {
 					case 0 :
-						System.out.println("0");
 						break;
 					case 1 :
 						gameGods[x][y].setIcon(gods[1]);
-						System.out.println("1");
 						break;
 					default :
-						System.out.println(x + y);
+						break;
 				}
 			}
 		}
